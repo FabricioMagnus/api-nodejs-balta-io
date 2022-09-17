204c0c239549d82d4e482491ab8ede28637340f3
Fabricio Pompeo Magnus <fabricio.magnus@softkuka.com.br>
2022-09-16 23:45:43 -0300

    connected: mongodb

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 const router = express.Router();
+
+//conecta ao banco
+mongoose.connect('mongodb+srv://FabricioMagnus:DE68rock@cluster0.bsutdbs.mongodb.net/test');
 
 //carrega as rotas
 const indexRoutes = require("./routes/index-route");
