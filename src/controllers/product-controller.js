9ba3c9e0d209d73d26d379fe04771dcd6e2610f3
Fabricio Pompeo Magnus <fabricio.magnus@softkuka.com.br>
2022-09-18 11:36:44 -0300

    add: CRUD

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,21 +1,79 @@
-"use strict";
+
+const Product = require('../models/Product')
+
+exports.get = (req, res, next)=>{
+  Product.find({active: true}, 'title price slug')
+  .then(data => {
+    res.status(200).send(data);
+}).catch(e => {
+    res.status(400).send(e);
+});    
+}
+
+exports.getBySlug = (req, res, next)=>{
+  Product.findOne({
+    active: true,
+    slug: req.params.slug
+  }, 'title description price slug tags')
+  .then(data => {
+    res.status(200).send(data);
+}).catch(e => {
+    res.status(400).send(e);
+});    
+}
+
+exports.getById = (req, res, next)=>{
+  Product
+  .findById(req.params.id)
+  .then(data => {
+    res.status(200).send(data);
+}).catch(e => {
+    res.status(400).send(e);
+});    
+}
+
+exports.getByTag = (req, res, next)=>{
+  Product
+  .find({
+    tags: req.params.tag,
+    active: true,
+  }, 'title description price slug tags')
+  .then(data => {
+    res.status(200).send(data);
+}).catch(e => {
+    res.status(400).send(e);
+});    
+}
 
 exports.post = (req, res, next) => {
-  res.status(200).send(req.body);
+    var product = new Product(req.body);
+    product.save()
+    .then(x => {
+        res.status(201).send({ mesage: "Produto cadastrado com sucesso!'" });
+    }).catch(e => {
+        res.status(400).send({
+            message: '[ERRO] Falha durante cadastro!',
+            data: e
+        });
+    });    
 };
-
 exports.put = (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({
-    id: id,
-    item: req.body,
-  });
+    Product.findByIdAndUpdate(req.params.id,{
+      $set: {
+        title:req.body.title,
+        slug:req.body.slug,
+        description:req.body.description,
+        price: req.body.price,
+      }
+    }).then(x=>{
+      res.status(200).send({message: 'Produto atualizado com sucesso!'})
+    }).catch(e=>{
+      res.status(400).send({
+        message: 'Falha ao atualizar o produto',
+        data: e
+      })
+    })
 };
-
 exports.delete = (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({
-    id: id,
-    item: req.body,
-  });
+    res.status(201).send(req.body);
 };
